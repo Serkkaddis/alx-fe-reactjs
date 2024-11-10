9e9ef42154e083ebb5f31e2f280010b87724a1ff
serkkaddis <serkaddistadesseb@gmail.com>
2024-11-10 19:04:53 +0300

    Applied Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '5px', marginTop: '20px' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
