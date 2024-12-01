73ba3fcdbda760715a67a05a799e67b2ebf7c67e
serkkaddis <serkaddistadesseb@gmail.com>
2024-12-01 23:28:12 +0300

    corrected styling  md:max-w-sm

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-        <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 rounded-lg shadow-lg">
+        <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm  md:max-w-sm mx-auto my-10 rounded-lg shadow-lg">
 
       {/*</div><div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img
