5bb6e9d4a880f181c7b6f514825e44ea3bedac4f
serkkaddis <serkaddistadesseb@gmail.com>
2024-12-16 09:47:42 +0300

    Set Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>GitHub User Search</h1>
+      </header>
+      <main>
+        <p>Start your search!</p>
+      </main>
+    </div>
+  );
+};
+
+export default App
