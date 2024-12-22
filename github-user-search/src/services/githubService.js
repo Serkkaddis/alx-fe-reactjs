b964e8703c7cc49d1a431ba2c3b7a48b507acf43
serkkaddis <serkaddistadesseb@gmail.com>
2024-12-22 11:44:54 +0300

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-export const fetchUserData = async (username, location, minRepos, page = 1) => {
-  let query = `q=${username}`;
-  if (location) query += `+location:${location}`;
-  if (minRepos) query += `+repos:>=${minRepos}`;
-  
-  const url = `https://api.github.com/search/users?${query}&page=${page}`;
-  const response = await axios.get(url);
-  return response.data.items;
-}
+export const fetchUserData = async (username, location, minRepos) => {
+  const queryParams = new URLSearchParams();
+
+  if (username) queryParams.append('q', `user:${username}`);
+  if (location) queryParams.append('location', location);
+  if (minRepos) queryParams.append('min_repos', minRepos);
+
+  try {
+    const response = await axios.get(`https://api.github.com/search/users?${queryParams.toString()}`);
+    return response.data.items; // Return the users' data from the search
+  } catch (error) {
+    throw new Error('Failed to fetch user data');
+  }
+};
