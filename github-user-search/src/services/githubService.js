5fbec7c9dfb577b3c8cbe2199be662a456e2a73f
serkkaddis <serkaddistadesseb@gmail.com>
2024-12-22 11:54:16 +0300

    updated services

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 
 export const fetchUserData = async (username, location, minRepos) => {
-  const queryParams = new URLSearchParams();
-
-  if (username) queryParams.append('q', `user:${username}`);
-  if (location) queryParams.append('location', location);
-  if (minRepos) queryParams.append('min_repos', minRepos);
-
   try {
-    const response = await axios.get(`https://api.github.com/search/users?${queryParams.toString()}`);
-    return response.data.items; // Return the users' data from the search
+    let url = `https://api.github.com/search/users?q=${username}`;
+
+    
+    if (location) {
+      url += `+location:${location}`;
+    }
+
+    
+    if (minRepos) {
+      url += `+repos:${minRepos}`;
+    }
+
+    const response = await axios.get(url);
+    return response.data.items; 
   } catch (error) {
-    throw new Error('Failed to fetch user data');
+    throw new Error('Error fetching user data');
   }
 };
